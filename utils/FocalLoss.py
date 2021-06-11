be46fb0e6355695e8b20093df56e6b40e62c98bc
ZQ.W <32612977+Chrisa142857@users.noreply.github.com>
2021-06-11 11:04:21 +0800

    Add files via upload

---
+++ utils/FocalLoss.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha])
+        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+
+        if logit.dim() > 2:
+            # N,C,d1,d2 -> N,C,m (m=d1*d2*...)
+            logit = logit.view(logit.size(0), logit.size(1), -1)
+            logit = logit.permute(0, 2, 1).contiguous()
+            logit = logit.view(-1, logit.size(-1))
+
+        target = target.view(-1)
+        # logpt = torch.log(input)
+        logpt = F.log_softmax(input)
+        logpt = logpt.gather(1,target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+        if self.alpha is not None:
+            if self.alpha.type()!=input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0,target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+        if self.size_average: return loss.mean()
+        else: return loss.sum()
+
+
+
+class BCEFocalLoss(torch.nn.Module):
+    """
+    二分类的Focalloss alpha 固定
+    """
+    def __init__(self, gamma=1, smoothed_label=0.95, reduction='elementwise_mean'):
+        super().__init__()
+        self.gamma = gamma
+        self.alpha = smoothed_label
+        self.reduction = reduction
+ 
+    def forward(self, _input, target):
+        # pt = torch.sigmoid(_input)
+        pt = _input
+        alpha = self.alpha
+        loss = - alpha * (1 - pt) ** self.gamma * target * torch.log(pt + 1e-9) - \
+               (1 - alpha) * pt ** self.gamma * (1 - target) * torch.log(1 - pt + 1e-9)
+        if self.reduction == 'elementwise_mean':
+            loss = torch.mean(loss)
+        elif self.reduction == 'sum':
+            loss = torch.sum(loss)
+        return loss
